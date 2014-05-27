4ff4f92fd118a93140fffa1f1d4a43d000934da7
Torrey Johnson <johnson.torrey@gmail.com>
2014-05-26 22:29:17 -0500

    turning off config assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.precompile += %w( vendor/respond.min.js )
-Rails.application.config.assets.precompile += %w( vendor/modernizr-2.7.1.min.js )
-Rails.application.config.assets.precompile += %w( default.js )
-Rails.application.config.assets.precompile += %w( works.js )
+# Rails.application.config.assets.precompile += %w( vendor/respond.min.js )
+# Rails.application.config.assets.precompile += %w( vendor/modernizr-2.7.1.min.js )
+# Rails.application.config.assets.precompile += %w( default.js )
+# Rails.application.config.assets.precompile += %w( works.js )
