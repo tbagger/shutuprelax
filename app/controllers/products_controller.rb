6e6dfac3b9689e7ecc81c159e8f32ceac395716f
Torrey Johnson <johnson.torrey@gmail.com>
2014-06-25 21:21:55 -0500

    Finshed Product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
-  def index
 
+  def index
+  	@products = Product.all
   end 
 
   def show
+  	@product = Product.find(params[:id])
 
   end
 end
