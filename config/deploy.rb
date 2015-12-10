275108ddfd3d1a275d158039ab033d321c65987f
Torrey Johnson <johnson.torrey@gmail.com>
2015-12-09 23:53:48 -0600

    fat fingerd

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :rvm_ruby_version, '2.2.3@shutuprelax'
+set :rvm_ruby_version, '2.2.3@shutuprelax2'
 
 namespace :deploy do
 
