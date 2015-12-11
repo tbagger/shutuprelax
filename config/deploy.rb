7e06f98bd0ebea4b04e40e8348dbc77e72b07961
Torrey Johnson <johnson.torrey@gmail.com>
2015-12-10 22:42:38 -0600

    another test

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 set :rvm_ruby_version, '2.2.3@shutuprelax2'
-set :passenger_restart_with_sudo, true
 
 namespace :deploy do
 
@@ -45,6 +44,7 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
