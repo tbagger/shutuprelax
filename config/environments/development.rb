f4e4e0052b324f5092a4ee408f49a15b293bad6d
Torrey Johnson <johnson.torrey@gmail.com>
2016-01-03 22:03:55 -0600

    testing new aws.yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-replay@example.com'}
+
+}
 end
