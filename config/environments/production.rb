91e079fb8b90d9fae776e41a04bd2a70fd22636d
Torrey Johnson <johnson.torrey@gmail.com>
2016-01-03 21:27:00 -0600

    added regions for aws again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-      region: "Oregan"
+      region: "Oregan",
     :s3_credentials => {
       :bucket => Rails.application.secrets.aws_s3_bucket,
       :access_key_id => Rails.application.secrets.aws_secret_access_key,
