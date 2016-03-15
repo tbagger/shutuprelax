a4bd5ab6344e5a18da7a9f39d2b3092c18946a17
Torrey Johnson <johnson.torrey@gmail.com>
2016-03-14 23:44:43 -0500

    mailer change from to shuguprelax

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,8 @@
 class ContactMailer < ActionMailer::Base
+  default from: 'mailer@shutuprelax.com'
 
   def contact_email(message)
     @message = message
-    mail(to: 'sarah@shutuprelax.com', from: @message.email, subject: 'Shutup Relax - Contact Message')
+    mail(to: 'sarah@shutuprelax.com', subject: 'Shutup Relax - Contact Message')
   end
 end
