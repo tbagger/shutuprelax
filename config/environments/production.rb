2c801b8a0197a1749e9aaa2aa7daa96b460ac21a
Torrey Johnson <johnson.torrey@gmail.com>
2016-03-14 21:42:09 -0500

    trying to get emails to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'shutuprelax.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -84,7 +87,7 @@ Rails.application.configure do
 
 
   # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
+  #   :address        => 'smtp.gmail.com',
   #   :port           => '587',
   #   :authentication => :plain,
   #   :user_name      => ENV['SENDGRID_USERNAME'],
